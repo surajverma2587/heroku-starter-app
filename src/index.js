b2b6fa9e1c3e7af98a8010110cabc6fe963f2a0d
Suraj Verma <surajverma@rentalcars.com>
2021-05-18 21:48:52 +0100

    initial code

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 3000;
+
+const app = express();
+
+const handleRequest = (req, res) => {
+  res.json({ message: "Hello" });
+};
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", handleRequest);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
